64338f8fea24de777fbdddc17b35b891cfbc6f8e
Logan Henrie <loghenrie@gmail.com>
2024-09-08 21:13:41 -0400

    move home-manager to configuration.nix

--- flake.nix
+++ flake.nix
@@ -61,17 +61,5 @@
       };
     };
 
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    homeConfigurations = {
-      "logan@loganthinkbook" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [
-          # > Our main home-manager configuration file <
-          ./home-manager/home.nix
-        ];
-      };
-    };
   };
 }
