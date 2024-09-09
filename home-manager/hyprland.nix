fa0f33217f40fbeced5b365a6aee3c472ed14887
Logan Henrie <loghenrie@gmail.com>
2024-09-09 13:51:52 +0000

    ???

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -1,7 +1,14 @@
-{
+{pkgs, ...}:{
   programs.kitty.enable = true;
   wayland.windowManager.hyprland = {
     enable = true;
   }
   home.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  home.pointerCursor = {
+    gtk.enable = true
+    package = pkgs.vimix-cursors;
+    name = "Vimix-cursors";
+    size = 16;
+  };
 }
