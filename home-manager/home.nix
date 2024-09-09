29cb3ea34dde920907cf12cdc5f1bd1aeecf8163
Logan Henrie <loghenrie@gmail.com>
2024-09-08 20:42:28 -0400

    updating to 24.05

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -47,13 +47,13 @@
 
   # TODO: Set your username
   home = {
-    username = "your-username";
-    homeDirectory = "/home/your-username";
+    username = "logan";
+    homeDirectory = "/home/logan";
   };
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
+  home.packages = with pkgs; [ helix ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
@@ -63,5 +63,5 @@
   systemd.user.startServices = "sd-switch";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "23.05";
+  home.stateVersion = "24.05";
 }
