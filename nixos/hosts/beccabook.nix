5164f417dd4766bffd2c31a5d1b10dd76a743682
Logan Henrie <loghenrie@gmail.com>
2024-09-29 23:06:07 +0000

    plymouth things

--- nixos/hosts/beccabook.nix
+++ nixos/hosts/beccabook.nix
@@ -9,7 +9,34 @@
     ../games.nix
   ];
 
-  boot.loader.systemd-boot.enable = true;
+  boot = {
+    plymouth = {
+      enable = true;
+      theme = "rings";
+      themePackages = with pkgs; [
+        (adi1090x-plymouth-themes.override {
+          selected_themes = ["rings"];
+        })
+      ];
+    };
+    loader.systemd-boot = {
+      editor = false;
+      enable = true;
+    };
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      #"splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+    loader.timeout = 0;
+  };
+
   services.logind.lidSwitch = "hybrid-sleep";
 
   networking.hostName = "beccabook";
