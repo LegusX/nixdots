b738f77eca1ddc1a4ec5c9e928ab6dded522189b
Logan Henrie <loghenrie@gmail.com>
2024-10-11 15:57:02 +0000

    merge hardwareconf into loganthinkbook.nix

--- nixos/hosts/loganthinkbook.nix
+++ nixos/hosts/loganthinkbook.nix
@@ -3,10 +3,12 @@
   inputs,
   outputs,
   config,
+  modulesPath,
   ...
 }: {
   imports = [
     inputs.home-manager.nixosModules.home-manager
+    (modulesPath + "/installer/scan/not-detected.nix")
     # outputs.nixosModules.gifWallpaper
   ];
 
@@ -55,4 +57,38 @@
   };
 
   programs.zsh.enable = true;
+
+  #Hardware configuration
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/b427321b-d2e3-4e31-9eea-bceabaf95b9b";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/7086-D615";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/0c3aac5a-c570-4e72-8be1-d1bf13e66eb6"; }
+    ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
 }
