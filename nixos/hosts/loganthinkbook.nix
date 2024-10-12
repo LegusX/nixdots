66f6982fb6dba5ee7a2905156d43a6616b163919
Logan Henrie <loghenrie@gmail.com>
2024-10-12 21:05:59 +0000

    vps things

--- nixos/hosts/loganthinkbook.nix
+++ nixos/hosts/loganthinkbook.nix
@@ -14,6 +14,8 @@
   ];
 
   networking.hostName = "loganthinkbook";
+  networking.networkmanager.enable = true;
+  services.resolved.enable = true;
 
   boot.loader.systemd-boot.enable = true;
   services.logind.lidSwitch = "hybrid-sleep";
@@ -23,6 +25,9 @@
       isNormalUser = true;
       shell = pkgs.zsh;
       extraGroups = ["wheel" "networkmanager" "audio"];
+      openssh.authorizedKeys.keys = [
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDagHYo50nLw9/0VtVI2WbjOz6oz7dM+D6YMsR8nHXKqfhErQBleuvcwnnhLUr3LjsyF3RVtYUf+WYSFnwz+0ZBJNtMdqLJg0OsPXsM1ugbZlx4ZVNb1uMm2vZ1cer0DbDqAQsWwVsB3Z+E5VbUmcpRNFFbRhR9bd5/b3qPV+wHoGriAIkcFHcJ1HKTksHcFh27MYPqBkNcOkPjAPk1Vtr53v/4JK7Q7Z6CyJagw/axuNEmGlXDDvfN8vPfwxsR47VOjjqk9l1rhLODl+XLZKXtTbRr3+mKcVirBhMX0fPZ+FxVVraH741tlVuWrlyuECfPiVRLWo+TH/P4asxZxpnp9exXJ/miofPRJemtwm076MJm7Tw6EVlJynfY34iaA4ZWJCPOQCSIj4krzAcTf1OhxGsmlwoNpVecYoUVYaLZ6eiAhb1YGbm15vGwpf3IW1vPF+v09OiR+E5XbwOTCxY6Kk5XEgjqAjRENdvK/qP70XcjLt+5zThFh9FDS8e+Ry0= logan@ryzen-shine"
+      ];
     };
     becca = {
       isNormalUser = true;
@@ -62,7 +67,7 @@
   #Hardware configuration
 
   networking.dnsExtensionMechanism = lib.mkForce false;
-  
+
   boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
--- nixos/hosts/vps.nix
+++ nixos/hosts/vps.nix
@@ -26,6 +26,7 @@
       initialPassword = "pleasepasswdnow";
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDg9USVFHk+6yHSutbFAa0KOTz71zZikc5TmGY3vnPic logan@nixos"
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDagHYo50nLw9/0VtVI2WbjOz6oz7dM+D6YMsR8nHXKqfhErQBleuvcwnnhLUr3LjsyF3RVtYUf+WYSFnwz+0ZBJNtMdqLJg0OsPXsM1ugbZlx4ZVNb1uMm2vZ1cer0DbDqAQsWwVsB3Z+E5VbUmcpRNFFbRhR9bd5/b3qPV+wHoGriAIkcFHcJ1HKTksHcFh27MYPqBkNcOkPjAPk1Vtr53v/4JK7Q7Z6CyJagw/axuNEmGlXDDvfN8vPfwxsR47VOjjqk9l1rhLODl+XLZKXtTbRr3+mKcVirBhMX0fPZ+FxVVraH741tlVuWrlyuECfPiVRLWo+TH/P4asxZxpnp9exXJ/miofPRJemtwm076MJm7Tw6EVlJynfY34iaA4ZWJCPOQCSIj4krzAcTf1OhxGsmlwoNpVecYoUVYaLZ6eiAhb1YGbm15vGwpf3IW1vPF+v09OiR+E5XbwOTCxY6Kk5XEgjqAjRENdvK/qP70XcjLt+5zThFh9FDS8e+Ry0= logan@ryzen-shine"
       ];
     };
   };
