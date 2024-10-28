10b3dab501a958055e9b7b47bdca286fddee0114
Logan Henrie <loghenrie@gmail.com>
2024-10-28 15:47:52 +0000

    adding email and oidc

--- nixos/services/mealie.nix
+++ nixos/services/mealie.nix
@@ -1,19 +1,34 @@
-{config, ...}: {
+{config, pkgs, ...}: {
   
   services.mealie = {
     enable = true;
     port = 9000;
+    credentialsFile = config.sops.templates."mealie.env".path;
+    package = pkgs.unstable.mealie
     settings = {
       ALLOW_SIGNUP = "false";
       MAX_WORKERS = "1";
       TZ = config.time.timeZone;
       BASE_URL = "https://mealie.legusx.dev";
+
+      # SMTP Setup
+      SMTP_HOST = "smtp.gmail.com";
+      
       # Can't make postgres work
       # DB_ENGINE = "postgres"; 
       # # Connect to pg over unix socket
       # POSTGRES_URL_OVERRIDE = "postgresql://mealie:@/mealie?host=/run/postgresql";
     };
   };
+
+  sops = {
+    secrets."mealie/smtp_pass" = {};
+    secrets."mealie/smtp_user" = {};
+    templates."mealie.env".content = ''
+      Environment=SMTP_USER=${config.sops.placeholder."mealie/smtp_user"} SMTP_FROM_EMAIL=${config.sops.placeholder."mealie/smtp_user"} SMTP_PASSWORD=${config.sops.placeholder."mealie/smtp_pass"}
+    ''
+  };
+  
 
   services.nginx.virtualHosts."mealie.legusx.dev" = {
     forceSSL = true;
@@ -22,7 +37,33 @@
     locations."/" = {
       proxyPass = "http://localhost:${builtins.toString config.services.mealie.port}";
     };
+    locations."/cloak/" = {
+      proxyPass = "http://localhost:${builtins.toString config.services.keycloak.settings.http-port}/cloak/";
+    };
   };
+
+  services.postgresql.enable = true;
+
+  services.keycloak = {
+    enable = true;
+
+    database = {
+      type = "postgresql";
+      createLocally = true;
+
+      username = "keycloak";
+      passwordFile = "${config.sops.secrets.keycloak.path}";
+    };
+
+    settings = {
+      hostname = "mealie.legusx.dev";
+      http-relative-path = "/cloak";
+      http-port = 38080;
+      proxy = "passthrough";
+      http-enabled = true;
+    };
+  };
+
   # Can't make postgres work
   # systemd.services = {
   #   mealie = {
