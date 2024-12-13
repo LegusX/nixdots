fdb80d2b3a500c6e7e21bf88c874d3f4ccb53e5b
Logan Henrie <loghenrie@gmail.com>
2024-12-12 19:44:54 -0500

    add disko

--- hosts/loganthinkbook.nix
+++ hosts/loganthinkbook.nix
@@ -110,4 +110,51 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # Disk setup
+  disko.devices = {
+    disk = {
+      main = {
+        device = "/dev/nvme0n1";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP={
+              priority = 1;
+              name = "ESP";
+              start = "1M";
+              end = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = ["umask=0077"];
+              };
+            };
+            root = {
+              end = "-16G";
+              content = {
+                type = "btrfs";
+                extraArgs = ["-f"];
+                mountpoint = "/";
+                mountOptions = ["noatime"];
+              };
+            };
+            swap = {
+              size="100%";
+              content = {
+                type = "swap";
+                discardPolicy = "both";
+                resumeDevice = true;
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+
 }
+
