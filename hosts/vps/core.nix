844df4ad868bd59b2eba9962f3a7a9f4fd4a499b
Logan Henrie <loghenrie@gmail.com>
2026-03-28 20:28:59 -0400

    moving homestead server to vps

--- hosts/vps/core.nix
+++ hosts/vps/core.nix
@@ -9,7 +9,7 @@
 }: {
   imports = [
     inputs.home-manager.nixosModules.home-manager
-    ./mealie.nix
+    # ./mealie.nix
     # ./nextcloud.nix 
     # ./actual.nix
     ../../modules/games/minecraft.nix
