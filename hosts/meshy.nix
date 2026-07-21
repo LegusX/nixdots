af611df363156ebec3b95262af7535b1e5bf68b6
Logan Henrie <loghenrie@gmail.com>
2026-07-21 09:09:41 -0400

    adding *arr to meshy

--- hosts/meshy.nix
+++ hosts/meshy.nix
@@ -37,6 +37,21 @@
       openFirewall = true;
     };
 
+    services.sonarr = {
+      enable = true;
+      openFirewall = true;
+    };
+
+    services.radarr = {
+      enable = true;
+      openFirewall = true;
+    };
+
+    services.overseerr = {
+      openFirewall = true;
+      enable = true;
+    };
+
     services.qbittorrent = {
       enable = true;
       openFirewall = true;
